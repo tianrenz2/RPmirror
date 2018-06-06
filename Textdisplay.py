ad1017396bb2aca9874e28f0516aba720ea685de
tianrenz2 <tianrenz@uci.edu>
2018-06-05 22:29:55 -0700

    Label working commit

--- Textdisplay.py
+++ Textdisplay.py
@@ -6,62 +6,73 @@ from PIL import ImageTk,Image
 import os
 
 
-root = Tk()
 
-def quit(*args):
-    root.destroy()
+class Mirror:
+    label = None
+    root = None
+    def __init__(self):
+        self.root = Tk()
+        self.label = Label(self.root)
+    def quit(self,*args):
+        self.root.destroy()
 
+    def createinterface(self):
+        self.root.attributes("-fullscreen", False)
+        self.root.geometry("{0}x{1}+0+0".format(self.root.winfo_screenwidth(), self.root.winfo_screenheight()))
 
-def createinterface():
-    root.attributes("-fullscreen", False)
-    root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+        self.root.configure(background='black')
+        self.root.bind("<Escape>", quit)
+        self.root.bind("x", quit)
+        self.root.after(1000, self.show_time)
 
-    root.configure(background='black')
-    root.bind("<Escape>", quit)
-    root.bind("x", quit)
-    root.after(1000, show_time)
+    def show_time(self):
+        tm.set(time.strftime("%H:%M:%S"))
+        self.root.after(1000, self.show_time)
 
-def show_time():
-    tm.set(time.strftime("%H:%M:%S"))
-    root.after(1000, show_time)
+    def show_date(self):
+        tm.set(datetime.datetime.today().strftime('%Y-%m-%d'))
+        self.root.after(1000, self.show_date)
 
-def show_date():
-    tm.set(datetime.datetime.today().strftime('%Y-%m-%d'))
-    root.after(1000, show_date)
+    def show_meme(self):
+        imageFile = "getout1.png"
+        im = Image.open(imageFile)
+        photo = ImageTk.PhotoImage(im)
+        # image = PhotoImage(file=photo)
+        self.label = Label(self.root,image=photo,borderwidth=135, highlightthickness=0, bg = 'black')
+        self.label.place(relx=0.5, rely=0.5, anchor = N)
+        self.label.image = photo
+        self.label.pack()
+        self.label.after(6000,self.clear_label)
 
-def show_meme():
-    imageFile = "getout1.png"
-    im = Image.open(imageFile)
-    photo = ImageTk.PhotoImage(im)
-    # image = PhotoImage(file=photo)
-    label = Label(root,image=photo,borderwidth=135, highlightthickness=0, bg = 'black')
-    label.place(relx=0.5, rely=0.5, anchor = N)
-    label.image = photo
-    label.pack()
+    def clear_label(self):
+        print ("clear_label")
+        self.label.pack_forget()
 
-def setweather():
-    return
+    def setweather(self):
+        return
 
 
 
 if __name__ == "__main__":
-    createinterface()
+
+    mirror = Mirror()
+    mirror.createinterface()
     fnt = font.Font(family='Apple Chancery', size=38, weight='bold')
     tm = StringVar()
     tm.set(time.strftime("%H:%M:%S"))
     dt = StringVar()
     dt.set(datetime.datetime.today().strftime('%Y-%m-%d'))
 
-    showtime = Label(root, textvariable=tm, font=fnt, fg="white", bg="black")
+    showtime = Label(mirror.root, textvariable=tm, font=fnt, fg="white", bg="black")
     showtime.place(relx=0.01, rely=0.13, anchor=W)
 
-    showdate = Label(root, textvariable=dt, font=fnt, fg="white", bg="black")
+    showdate = Label(mirror.root, textvariable=dt, font=fnt, fg="white", bg="black")
     showdate.place(relx=0.11, rely=0.13, anchor=W)
 
-    title = Label(root, text="Anteater Mirror",fg = "white", bg = "black", font = fnt)
+    title = Label(mirror.root, text="Anteater Mirror",fg = "white", bg = "black", font = fnt)
     title.place(relx=0.01, rely=0.01)
 
-    show_meme()
+    mirror.show_meme()
 
 
-    root.mainloop()
+    mirror.root.mainloop()
